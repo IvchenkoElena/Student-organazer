c59770794d36905242a548d9cce7ac3e6809c3f6
kamo111 <kamo19.989king@gmail.com>
2025-12-22 02:51:16 +0300

    Доп задание 4

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+import model.Student;
+import model.StudentBuilder;
+import java.util.List;
+import java.util.Arrays;
+import counter.ElementCounter;
+
+public class Main
+{
+    public static void main(String[] args) {
+      StuOrgApp app = new StuOrgApp();
+      app.run();
+                List<Integer> numbers = Arrays.asList
+                        (
+                        1, 2, 3, 4, 2, 5, 2, 6, 7, 2, 8, 2, 9
+                );
+
+                ElementCounter.countOccurrences(numbers, 2, 3);
+            }
+        }
