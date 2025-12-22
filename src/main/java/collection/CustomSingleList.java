1392077b6e84ab07c6c97d0ef584bdb3750ba063
Kirill Stankevich <stankevichkirill6@gmail.com>
2025-12-22 19:38:18 +0300

    Создан класс для заполнения коллекции и сама кастомная коллекция

---
+++ src/main/java/collection/CustomSingleList.java
@@ -0,0 +1,97 @@
+package collection;
+
+import java.util.*;
+
+
+public class CustomSingleList<T> extends AbstractList<T> {
+    private Node head;
+    private int size;
+
+    private class Node {
+        private Node next;
+        private final T data;
+
+
+        public Node(T data) {
+            this.data = data;
+
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "next=" + next +
+                    ", data=" + data +
+                    '}';
+        }
+    }
+
+
+    public CustomSingleList() {
+        size = 0;
+
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+
+    @Override
+    public boolean add(T t) {
+        Node node = new Node(t);
+        size++;
+        if (head == null) {
+            head = node;
+
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        return true;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("индекс отрицательный или больше размера списка " + index);
+        }
+        if (head == null) {
+            throw new IllegalStateException("Список пуст");
+        }
+        Node current = head;
+        if (index == 0) {
+            return current.data;
+        }
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof CustomSingleList<?> that)) return false;
+        if (!super.equals(o)) return false;
+        return size == that.size && Objects.equals(head, that.head);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), head, size);
+    }
+
+    @Override
+    public String toString() {
+        return "CustomSingleList{" +
+                "head=" + head +
+                ", size=" + size +
+                '}';
+    }
+
+
+}
--- src/test/java/TestApp.java
+++ src/test/java/TestApp.java
@@ -1,3 +1,5 @@
+import collection.CollectionFiller;
+import collection.CustomSingleList;
 import model.Student;
 import model.StudentBuilder;
 import org.junit.jupiter.api.Test;
@@ -5,23 +7,21 @@ import sorting.SortService;
 import sorting.SortType;
 import validation.Validation;
 
-import java.awt.*;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestApp {
-    private Validation validation = new Validation();
+
 
     @Test
     void notValidAllDataInput() {
-        assertFalse(validation.isValidInputData(1000, 10.0, "656776755656"));
+        assertFalse(Validation.isValidInputData(1000, 10.0, "656776755656"));
     }
 
     @Test
-    void validAllDataInput(){
-        assertTrue(validation.isValidInputData(300, 4.3, "LM887963"));
+    void validAllDataInput() {
+        assertTrue(Validation.isValidInputData(300, 4.3, "LM887963"));
     }
 
     @Test
@@ -63,5 +63,45 @@ public class TestApp {
         list = Arrays.asList(student, student2, student3, student4, student5, student6);
         SortService.sort(list, SortType.INCREDIBLE);
         assertEquals(list.getLast(), student6);
+    }
+
+    @Test
+    void testFillCollection() {
+        Student student = new StudentBuilder()
+                .setGroupNumber(10)
+                .setAverageGrade(19)
+                .setRecordBookNumber("jhg")
+                .build();
+
+        Student student2 = new StudentBuilder()
+                .setGroupNumber(5)
+                .setAverageGrade(4.9)
+                .setRecordBookNumber("435456")
+                .build();
+        Student student3 = new StudentBuilder()
+                .setGroupNumber(3)
+                .setAverageGrade(2)
+                .setRecordBookNumber("nvv")
+                .build();
+        Student student4 = new StudentBuilder()
+                .setGroupNumber(1)
+                .setAverageGrade(2.1)
+                .setRecordBookNumber("Gc")
+                .build();
+        Student student5 = new StudentBuilder()
+                .setGroupNumber(0)
+                .setAverageGrade(2.1)
+                .setRecordBookNumber("fgdfgds")
+                .build();
+        Student student6 = new StudentBuilder()
+                .setGroupNumber(0)
+                .setAverageGrade(0)
+                .setRecordBookNumber("dfsgfgsfg")
+                .build();
+        CustomSingleList<Student> list = CollectionFiller.fillStudents(student, student2, student3, student4, student5, student6);
+        assertFalse(list.isEmpty());
+
+
+
     }
 }
