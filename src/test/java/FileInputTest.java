61d11a739c8a780affbe3c82bc94e8c2e0f6de56
IvchenkoElena <pesok72@mail.ru>
2025-12-26 06:47:20 +0500

    Написаны тесты для FileInputStrategy. При заполнении через стрим невалидные строки пропускаются

--- src/test/java/FileInputTest.java
+++ src/test/java/FileInputTest.java
@@ -1,2 +1,14 @@
+import input.FileInputStrategy;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FileInputTest {
+    private final FileInputStrategy strategy = new FileInputStrategy();
+
+    @Test
+    void equalsFileInputData() {
+        assertEquals(7, strategy.read(7).size());
+        assertEquals("ZK123456", strategy.read(1).getFirst().getRecordBookNumber());
+    }
 }
